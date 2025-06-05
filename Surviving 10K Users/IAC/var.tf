74148b5394741a8c85e7ed1e06384ab718ad130e
zudonu <osomudeya.z@9japay.com>
2025-06-05 19:52:45 +0100

    added

---
+++ Surviving 10K Users/IAC/var.tf
@@ -0,0 +1,11 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-west-2"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.medium"
+}
